7c8c28e41027afa60477971ae1cc17afbbd946ce
Dhanushit419 <dhanushit419@gmail.com>
2023-07-19 18:17:51 +0530

    added search function and product selling page

--- FrontEnd/src/Pages/register.js
+++ FrontEnd/src/Pages/register.js
@@ -8,10 +8,41 @@ import mobile from '../images/mobile.png';
 import email from '../images/email.png';
 import { Button } from "@mui/material";
 import TextField from '@mui/material/TextField';
+import axios from "axios";
 
 
 
-export default function login(){
+export default function Register(){
+
+
+    const navigate =useNavigate();
+    const [userDetails,setuserDetails]=useState({name:"",email:"",mobile:"",pwd:""});
+  
+  function  UpdateInfo(e){
+      setuserDetails({...userDetails,[e.target.id]:e.target.value})
+  }
+
+  const RegisterUser = () =>{
+    axios({
+        url:"http://localhost:3001/register",
+        method:"POST",
+        params:userDetails
+    })
+    .then((res)=>{
+        if(res.data.newUser){
+            alert("User Registered Successfully ! Please Login to continue :)")
+            navigate("/login")
+        }
+        else{
+            alert("Email Already Registered! Try with Different Email or Login to Continue :)")
+
+        }
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+  }
+  
     return (
         <div className="login">
     <header className='header-home'>
@@ -33,39 +64,37 @@ export default function login(){
             <h1 style={{padding:"0 30px"}}>Register Page</h1>
         </div>
         <br/>
-        <form action='/login'>
 
          <div style={{display:"flex",justifyContent:"space-around"}}  >   
             <img alt='' src={user} height={30} className='user'/>
-            <TextField id="outlined-basic" size="small" type="text" label="Fullname" variant="outlined" required/>
+            <TextField id="name" value={userDetails.name} onChange={UpdateInfo} label="Fullname" size="small" type="text"  variant="outlined" required/>
         </div>
         <br/>
         <div style={{display:"flex",justifyContent:"space-around"}}  >   
             <img alt="" src={mobile} height={30} className='user'/>
-            <TextField id="outlined-basic"size="small" type="phone" label="Mobile Number" variant="outlined" required />
+            <TextField id="mobile" value={userDetails.mobile} onChange={UpdateInfo} label="Mobile Number" size="small" type="phone"  variant="outlined" required />
         </div>
         <br/>
         <div style={{display:"flex",justifyContent:"space-around"}}  >   
             <img alt='' src={email} height={30} className='user'/>
-            <TextField id="outlined-basic"size="small" type="email" label="Email" variant="outlined" required/>
+            <TextField id="email" value={userDetails.email} onChange={UpdateInfo} label="Email" size="small" type="email"  variant="outlined" required/>
         </div>
         <br/>
         <div  style={{display:"flex",justifyContent:"space-around"}}>
             <img alt=''  src={pass} height={30} />
-            <TextField id="outlined-basic"size="small" type="password" label="Password" variant="outlined" required/>
+            <TextField id="pwd" value={userDetails.pwd} onChange={UpdateInfo} size="small" type="password" label="Password" variant="outlined" required/>
         </div>
         <br></br>
         <div  style={{display:"flex",justifyContent:"space-around"}}>
             <img alt='' src={pass} height={30} />
-            <TextField id="outlined-basic"size="small" type="password" label="Confirm Password" variant="outlined"required />
+            <TextField id="confirm-pwd" label="Confirm Password" size="small" type="password"  variant="outlined"required />
         </div>
         <br />
-        <Button variant="contained" className='submit-button'type="submit">Register</Button>         <br></br>
+        <Button onClick={RegisterUser} variant="contained" className='submit-button' type="submit">Register</Button>         <br></br>
          <br />
          <br/>
          <div className='register'><p>Already Registered ?</p><a href='/login'>Login</a></div>
 
-        </form>
 
     </div>
          </div> 
